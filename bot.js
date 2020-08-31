311d66e81c6e1b227b6b6acaaad4f19de30d682d
unknown <aishwaryababu1212@gmail.com>
2020-08-31 18:28:42 +0530

    basic app done

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '1330014271:AAFoYwCF54yV1JTVtxIt8gqOxyUxDqyjt50';
 var bot = new TelegramBot(token,{polling: true});
 var request = require('request');
+const e = require('express');
+var currentlyChatting = false,alreadySent = false;
 
 bot.onText(/\/echo (.+)/, function(msg, match){
     var chatID = msg.chat.id;
@@ -9,13 +11,13 @@ bot.onText(/\/echo (.+)/, function(msg, match){
     bot.sendMessage(chatID,echo);
 });
 
-bot.onText(/\/start (.+)/, function(msg,match){
+bot.onText(/^\/start$/, function(msg,match){
     var chatID = msg.chat.id;
-    console.log("WAEdgfhg");
-    //bot.sendMessage(chatID, "Hello there! I'm a chatbot. Please use _/movie [movie_name]_ to get details about a movie.",{parse_mode:"Markdown"})
+    bot.sendMessage(chatID, "Hello there! I'm a chatbot. \nUse the following commands!  \n*/movie <movie-name>* : get info about a movie. \n*/recommend <movie-name>* : suggest similar movies.",{parse_mode:"Markdown"})
 });
 
-bot.onText(/\/movie(.+)/, function(msg,match){
+
+bot.onText(/\/movie(.+)/, function (msg,match){
     var movie = match[1];
     var chatID = msg.chat.id;
     movie = movie.substr(1,movie.length);
@@ -36,3 +38,65 @@ bot.onText(/\/movie(.+)/, function(msg,match){
         }
     });
 });
+
+bot.onText(/\/recommend(.+)/, function(msg,match){
+    var chatID = msg.chat.id;
+    var movie = match[1];
+    currentlyChatting = true;
+    var currInd = 0;
+    sendRecommendations(chatID,0,movie);
+    setTimeout(function(){
+        ask(chatID);
+    },5000);
+    bot.on('message',(msg) => {
+        if(currentlyChatting) {
+            if(msg.text.toString().toLowerCase() == "yes") {
+                currInd+=5;
+                sendRecommendations(chatID,currInd,movie);
+                setTimeout(function(){
+                    ask(chatID);
+                },5000);
+            } else if(msg.text.toString().toLowerCase() == "no"){
+                bot.sendMessage(msg.chat.id, "Cool! Hope you found what you were looking for.");
+                currentlyChatting = false;
+            }
+        }
+    });
+    
+});
+
+function sendRecommendations(chatID,currNum,movie) {
+    request('https://api.themoviedb.org/3/search/movie?api_key=fc2e228c666880dd9a0a900e34947de9&language=en-US&query='+movie+'&page=1&include_adult=false', function(err,response,body){
+        if(!err && response.statusCode == 200) {
+            var res = JSON.parse(body);
+            var id = res.results[0].id;
+            request('https://api.themoviedb.org/3/movie/'+id+'/recommendations?api_key=fc2e228c666880dd9a0a900e34947de9&language=en-US', function(err,response,body){
+                var resp = JSON.parse(body);
+                var num = 5;
+                if(resp.results.length-currNum < 5)
+                    num = resp.results.length-currNum;
+                for(var i=currNum;i<currNum+num;i++) {
+                    bot.sendPhoto(chatID,'http://image.tmdb.org/t/p/w185//'+resp.results[i].poster_path,{caption:"*Title:* " + resp.results[i].title + "\n*Plot:* " + resp.results[i].overview, parse_mode:'Markdown'});
+                }
+            });
+        }
+    });
+    alreadySent = false;
+}
+
+function ask(chatID) {
+    const opts = {
+        reply_markup: {
+            resize_keyboard: true,
+            one_time_keyboard: true,
+            keyboard: [ ['Yes'],['No'] ]
+        }
+    };
+    if(alreadySent == false) {
+        bot.sendMessage(chatID, "Do you want more suggestions?", opts); 
+        alreadySent = true;
+    }
+    else {
+
+    }
+}
